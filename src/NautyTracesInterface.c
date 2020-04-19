3dbd4fbe1fc0d699d0442ef670b3884f46bfba8e
Max Horn <max@quendi.de>
2020-04-19 23:42:15 +0200

    Change #include "src/compiled.h" to just "compiled.h"
    This will allow future GAP versions to simplify their GAP_CPPFLAGS,
    which currently need to contain `-I$(abs_srcdir)` just to support this
    old use case. Also, this in turn makes it easier to have package
    compatible with future installed version of GAP, which have headers in
    PREFIX/include/gap/ so there is no `src` there (unless downstream
    packagers jump through hoops to ensure that, e.g. by adding a special
    symlink).
    
    Note: we could also #include "gap_all.h" but that would require GAP >= 4.11.

--- src/NautyTracesInterface.c
+++ src/NautyTracesInterface.c
@@ -2,7 +2,7 @@
  * NautyTracesInterface: An interface to nauty
  */
 
-#include "src/compiled.h" /* GAP headers */
+#include "compiled.h" /* GAP headers */
 #include <naugroup.h>
 #include <nautinv.h>
 #include <nauty.h>
