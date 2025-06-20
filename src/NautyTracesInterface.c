75cf4e55feaff7d3b3bce0624d7290bc8b0ec56b
Max Horn <max@quendi.de>
2025-06-20 12:59:12 +0200

    Replace compiled.h by gap_all.h (#52)
    No functional change for this package but makes it future-proof.

--- src/NautyTracesInterface.c
+++ src/NautyTracesInterface.c
@@ -2,7 +2,7 @@
  * NautyTracesInterface: An interface to nauty
  */
 
-#include "compiled.h" /* GAP headers */
+#include "gap_all.h" /* GAP headers */
 #include <naugroup.h>
 #include <nautinv.h>
 #include <nauty.h>
